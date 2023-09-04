6c1595e4b32469a5826b6e5e1225d5af24dd8d2b
stormphoenix <stormphoenix.hzau@hotmail.com>
2023-09-05 00:44:02 +0800

    Add personal exercises.
    (cherry picked from commit f3c672bf9965b6f986430df558152784d10d7fbd)

---
+++ Lesson/Scripts/BuildShaders.bat
@@ -0,0 +1,2 @@
+D:/MainWorkspace/Vulkan/Bin/glslc.exe ../Shaders/ShaderDrawTriangle.vert -o ../Build/Shaders/ShaderDrawTriangle_Vert.spv
+D:/MainWorkspace/Vulkan/Bin/glslc.exe ../Shaders/ShaderDrawTriangle.frag -o ../Build/Shaders/ShaderDrawTriangle_Frag.spv
