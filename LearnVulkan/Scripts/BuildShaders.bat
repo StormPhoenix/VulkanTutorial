2a0a95edbdd461d9073d2327b66767ab2dede8bc
jiaxianwang <jiaxianwang@tencent.com>
2023-09-05 10:17:01 +0800

    修改文件路径。
    (cherry picked from commit 688c17b437b399c84fd870cb8518490d6ab9797f)

---
+++ LearnVulkan/Scripts/BuildShaders.bat
@@ -0,0 +1,2 @@
+%~dp0glslc.exe ./Shaders/ShaderDrawTriangle.vert -o ./Shaders/ShaderDrawTriangle_Vert.spv
+%~dp0glslc.exe ./Shaders/ShaderDrawTriangle.frag -o ./Shaders/ShaderDrawTriangle_Frag.spv
--- Lesson/Scripts/BuildShaders.bat
+++
@@ -1,2 +0,0 @@
-D:/MainWorkspace/Vulkan/Bin/glslc.exe ./Shaders/ShaderDrawTriangle.vert -o ./Shaders/ShaderDrawTriangle_Vert.spv
-D:/MainWorkspace/Vulkan/Bin/glslc.exe ./Shaders/ShaderDrawTriangle.frag -o ./Shaders/ShaderDrawTriangle_Frag.spv
